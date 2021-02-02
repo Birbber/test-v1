dc689f562f76304ea990ebc7b091bf01a9d9f92e
anastenko <78078725+anastenko@users.noreply.github.com>
2021-02-02 12:00:54 +0300

    Create v1.js

---
+++ v1.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         script doing nothing
+// @version      0.3
+// @description  what a shame
+// @match        *://*/*
+// @grant        unsafeWindow
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+
+})();
